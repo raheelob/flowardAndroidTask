a8831d0b641fe257778b92f8176c740794aa0abf
raheelob <raheel.ob@gmail.com>
2022-10-16 22:11:54 +0400

    Repository Unit Test
    Fetch repo.
    1- Unit test for users.
    2- Unit test for posts

---
+++ app/src/test/java/com/floward/androidtask/view/network/FetchUsersAndPostTest.kt
@@ -0,0 +1,70 @@
+package com.floward.androidtask.view.network
+
+import com.floward.androidtask.BuildConfig
+import com.floward.androidtask.data.api.APIsCollection
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@RunWith(JUnit4::class)
+class FetchUsersAndPostTest {
+    private lateinit var service: APIsCollection
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(APIsCollection::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String) {
+        javaClass.classLoader?.let {
+            val inputStream = it.getResourceAsStream(fileName)
+            val source = inputStream.source().buffer()
+            val mockResponse = MockResponse()
+            mockResponse.setBody(source.readString(Charsets.UTF_8))
+            server.enqueue(mockResponse)
+        }
+    }
+
+    @Test
+    fun getUserListRequest() {
+        runBlocking {
+            enqueueMockResponse("UsersResponse.json")
+            val responseBody = service.getUsersList()
+            assertTrue(responseBody.toString().isNotEmpty())
+            val request = server.takeRequest()
+            assertTrue(request.path.equals("/users"))
+        }
+    }
+
+    @Test
+    fun getPostRequest() {
+        runBlocking {
+            enqueueMockResponse("PostsResponse.json")
+            val responseBody = service.getUserPost()
+            assertTrue(responseBody.toString().isNotEmpty())
+            val request = server.takeRequest()
+            assertTrue(request.path.equals("/posts"))
+        }
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+}
---
+++ app/src/test/java/com/floward/androidtask/view/viewmodel/UserViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.floward.androidtask.view.viewmodel
+
+import org.junit.Assert.*
+
+class UserViewModelTest
